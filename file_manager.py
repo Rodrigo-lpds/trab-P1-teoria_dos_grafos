7b5c50b547def18b47e9a161a6f6023a859924a8
Rodrigo Lucas <rodrigo.lpdsilva@gmail.com>
2021-12-18 16:27:07 -0300

    add function to get vertex quantity

--- file_manager.py
+++ file_manager.py
@@ -4,3 +4,9 @@ def read_graph_file(file_name):
     """
     with open(file_name, 'r') as file:
         return file.readlines()
+
+def get_vertex_quantity(graph): 
+    """
+    Returns the vertex of the graph.
+    """
+    return graph[0].rstrip("\n")
