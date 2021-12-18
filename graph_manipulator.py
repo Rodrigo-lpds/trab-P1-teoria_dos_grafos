ea9b101b0d6f8a2e681dee61884a10258fb90281
Rodrigo Lucas <rodrigo.lpdsilva@gmail.com>
2021-12-18 17:42:04 -0300

    remove unnecessary if

--- graph_manipulator.py
+++ graph_manipulator.py
@@ -31,7 +31,6 @@ def adjacency_matrix(graph):
     # create the adjacency matrix
     adj_matrix = [[0 for i in range(len(unique_values))] for j in range(len(unique_values))]
     for line in graph[1:]:
-        if line[0] != '#':
             adj_matrix[unique_values.index(line[0])][unique_values.index(line[2])] = 1
             adj_matrix[unique_values.index(line[2])][unique_values.index(line[0])] = 1
     return adj_matrix
@@ -52,11 +51,10 @@ def adjacency_list(graph):
     # create the adjacency list
     adj_list = [[] for i in range(len(unique_values))]
     for line in graph[1:]:
-        if line[0] != '#':
             adj_list[unique_values.index(line[0])].append(line[2])
             adj_list[unique_values.index(line[2])].append(line[0])
     return adj_list
 
 
 print('\n'.join([''.join(['{:4}'.format(item) for item in row]) 
-      for row in adjacency_list(graph)]))
+     for row in adjacency_list(graph)]))
