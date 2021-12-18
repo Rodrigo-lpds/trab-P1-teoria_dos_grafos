c6a7d9d896294ea5db1eeb01c08d5b4b6bea6e50
Rodrigo Lucas <rodrigo.lpdsilva@gmail.com>
2021-12-18 17:00:30 -0300

    remove graph manipulation of file manager

--- file_manager.py
+++ file_manager.py
@@ -4,15 +4,3 @@ def read_graph_file(file_name):
     """
     with open(file_name, 'r') as file:
         return file.read().splitlines()
-
-def get_vertex_quantity(graph): 
-    """
-    Returns the vertex of the graph.
-    """
-    return graph[0].rstrip("\n")
-
-def get_edge_quantity(graph):
-    """
-    Returns the edge of the graph.
-    """
-    return len(graph) - 1	
