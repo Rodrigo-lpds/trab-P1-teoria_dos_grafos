c12dbabb92ffe7eadf63dc8ad506e453134f383c
Rodrigo Lucas <rodrigo.lpdsilva@gmail.com>
2021-12-18 16:37:41 -0300

    remove newline of elements

--- file_manager.py
+++ file_manager.py
@@ -3,7 +3,7 @@ def read_graph_file(file_name):
     Reads a graph file and returns a list of all the lines in the file.
     """
     with open(file_name, 'r') as file:
-        return file.readlines()
+        return file.read().splitlines()
 
 def get_vertex_quantity(graph): 
     """
