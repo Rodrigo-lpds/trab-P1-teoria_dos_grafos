46a011db9724170c170295d23c8b129427e8d68e
Rodrigo Lucas <rodrigo.lpdsilva@gmail.com>
2021-12-18 16:17:47 -0300

    add a file manager to read txt files

---
+++ file_manager.py
@@ -0,0 +1,6 @@
+def read_graph_file(file_name):
+    """
+    Reads a graph file and returns a list of all the lines in the file.
+    """
+    with open(file_name, 'r') as file:
+        return file.readlines()
