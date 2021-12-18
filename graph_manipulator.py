9ae9cd427814f5753e692be294693ddc879048ed
Rodrigo Lucas <rodrigo.lpdsilva@gmail.com>
2021-12-18 17:38:47 -0300

    add adjacency_list

--- graph_manipulator.py
+++ graph_manipulator.py
@@ -36,5 +36,27 @@ def adjacency_matrix(graph):
             adj_matrix[unique_values.index(line[2])][unique_values.index(line[0])] = 1
     return adj_matrix
 
+def adjacency_list(graph):
+    """
+    Returns the adjacency list of the graph.
+    """
+    adj_list = []
+    unique_values = set()
+    # get the unique values of the graph
+    for line in graph:
+        adj_list.append(line.split())
+        unique_values.update(adj_list[-1])
+    unique_values = list(unique_values)
+    unique_values.sort()
+
+    # create the adjacency list
+    adj_list = [[] for i in range(len(unique_values))]
+    for line in graph[1:]:
+        if line[0] != '#':
+            adj_list[unique_values.index(line[0])].append(line[2])
+            adj_list[unique_values.index(line[2])].append(line[0])
+    return adj_list
+
+
 print('\n'.join([''.join(['{:4}'.format(item) for item in row]) 
-      for row in adjacency_matrix(graph)]))
+      for row in adjacency_list(graph)]))
