58c38c194037f4b086ba6bab5003e246b915930b
Rodrigo Lucas <rodrigo.lpdsilva@gmail.com>
2021-12-18 16:30:52 -0300

    add function to get edge quantity

--- file_manager.py
+++ file_manager.py
@@ -10,3 +10,9 @@ def get_vertex_quantity(graph):
     Returns the vertex of the graph.
     """
     return graph[0].rstrip("\n")
+
+def get_edge_quantity(graph):
+    """
+    Returns the edge of the graph.
+    """
+    return len(graph) - 1	
