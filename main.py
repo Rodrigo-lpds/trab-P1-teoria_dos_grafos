e25f00431770ea2174af6f92438aa97504667814
Rodrigo Lucas <rodrigo.lpdsilva@gmail.com>
2021-12-26 16:06:30 -0300

    import adjascencyvector and adjascency list adjascency matrix

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import file_manager as fm
 import  graph_manager as gm
 
 from representations.adjascencyList import AdjList
+from representations.adjascencyMatrix import AdjMatrix
+from representations.adjascencyVector import AdjVector
 
 if __name__ == "__main__":
     graph_example = fm.read_graph_file('graph_example.txt')
     V = int(gm.get_vertex_quantity(graph_example))
     # Create graph and edges
-    graph = AdjList(V+1)
-    for line in graph_example[1:]:
-        line = line.split(' ')
-        graph.add_edge(int(line[0]), int(line[1]))
-    graph.print_agraph()
+    graph = AdjVector(V)
+    #graph.adjacency_vector(graph_example)
+    graph.print_agraph(graph_example)
