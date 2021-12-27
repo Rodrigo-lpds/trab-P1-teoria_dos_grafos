b3868b73dfbecbde8a565f1c341e2c7f117c7d2f
Rodrigo Lucas <rodrigo.lpdsilva@gmail.com>
2021-12-27 16:08:20 -0300

    fix adjacencyMatrix

--- representations/adjascencyMatrix.py
+++ representations/adjascencyMatrix.py
@@ -1,17 +1,31 @@
 #Adjascency Matrix representation in Python
 class AdjMatrix:
-    def __init__(self, num):
+    def __init__(self, num, graph):
         self.V = num
-        self.graph = [[0 for i in range(self.V)] for j in range(self.V)]
+        self.graph = graph       
 
-    # Add edges
-    def add_edge(self, s, d):
-        self.graph[s][d] = 1
-        self.graph[d][s] = 1
+    
+    def adjacency_matrix(self):
+        """
+        Returns the adjacency matrix of the graph.
+        """
+        adj_matrix = []
+        unique_values = set()
+        # get the unique values of the graph
+        for line in self.graph:
+            adj_matrix.append(line.split())
+            unique_values.update(adj_matrix[-1])
+        unique_values = list(unique_values)
+        unique_values.sort()
 
-    # Print the graph
+        # create the adjacency matrix
+        adj_matrix = [[0 for i in range(len(unique_values))] for j in range(len(unique_values))]
+        for line in self.graph[1:]:
+                adj_matrix[unique_values.index(line[0])][unique_values.index(line[2])] = 1
+                adj_matrix[unique_values.index(line[2])][unique_values.index(line[0])] = 1
+        return adj_matrix
+    
     def print_agraph(self):
-        for i in range(self.V):
-            for j in range(self.V):
-                print(self.graph[i][j], end=" ")
-            print("\n")
+        # Print the graph
+        print('\n'.join([''.join(['{:4}'.format(item) for item in row]) 
+        for row in self.adjacency_matrix()]))
