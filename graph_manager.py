535f7eb0123189455e7377fad12270c9040405cb
Rodrigo Lucas <rodrigo.lpdsilva@gmail.com>
2021-12-26 22:42:51 -0300

    move estimate_diameter to graph manager

--- graph_manager.py
+++ graph_manager.py
@@ -1,3 +1,8 @@
+import math
+import random
+
+from trees.breadth_first_search import breadth_first_search
+
 def get_vertex_quantity(graph): 
     """
     Returns the vertex of the graph.
@@ -9,4 +14,19 @@ def get_edge_quantity(graph):
     Returns the edge of the graph.
     """
     return len(graph) - 1	
- 
+
+def estimate_diameter(graph,vertex_quantity):
+        """
+        Estimate the diameter of a graph.
+        """
+        diameter = 0
+        k = math.log(vertex_quantity,2)
+        k = math.pow(k,2)
+        for i in range(int(k)):
+            random_vertex = random.randint(1,vertex_quantity)
+            tree_bfs = breadth_first_search(graph, str(random_vertex))
+            tree_bfs.bfs()
+            max_distance = tree_bfs.get_max_distance()
+            if max_distance > diameter:
+                diameter = max_distance
+        return diameter
--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import math
-import random
-
 import file_manager as fm
 import  graph_manager as gm
 
@@ -11,29 +8,24 @@ from representations.adjascencyVector import AdjVector
 from trees.breadth_first_search import breadth_first_search
 from trees.depth_first_search import depth_first_search
 
-def estimate_diameter(graph,vertex_quantity):
-        """
-        Estimate the diameter of a graph.
-        """
-        diameter = 0
-        k = math.log(vertex_quantity,2)
-        k = math.pow(k,2)
-        for i in range(int(k)):
-            random_vertex = random.randint(1,vertex_quantity)
-            tree_bfs = breadth_first_search(graph, str(random_vertex))
-            tree_bfs.bfs()
-            max_distance = tree_bfs.get_max_distance()
-            if max_distance > diameter:
-                diameter = max_distance
-        return diameter
-        
+from connected_graph.connected_graph import connected_graph
+
+
 if __name__ == "__main__":
-    graph_example = fm.read_graph_file('cases/grafo_1.txt')
+    graph_example = fm.read_graph_file('cases/grafo_2.txt')
     V = int(gm.get_vertex_quantity(graph_example))
-    #tree_bfs = breadth_first_search(graph_example, '61')
+    #tree_bfs = breadth_first_search(graph_example, '6')
     #tree_bfs.bfs()
+    #tree_bfs.print_result()
+    conn = connected_graph(graph_example, V)
+    print(conn.get_connected_graphs())
+    conn.print_connected_graphs()
+    #print(tree_bfs.get_connected_graphs())
+
+    #print graph to pdf
+
     #print(tree_bfs.get_max_distance())
-    print(estimate_diameter(graph_example,V))
+    #print(estimate_diameter(graph_example,V))
     #tree_bfs.write_tree()
     #print(tree_bfs.get_distance('1'))
 
